105f3f77e7795e1aff8384c5c64c0ce4236a48b7
sam0verfl0w <stales@student.42angouleme.fr>
2023-05-10 00:09:31 +0200

    rework

--- inc/decoder.h
+++
@@ -1,6 +0,0 @@
-#ifndef DECODER_H
-#define DECODER_H
-
-void	say_hello(void);
-
-#endif
--- src/say_hello.c
+++
@@ -1,7 +0,0 @@
-#include "decoder.h"
-#include <stdio.h>
-
-void	say_hello(void)
-{
-	printf("hello world\n");
-}
